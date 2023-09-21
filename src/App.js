86655891605db45f1dfcaf23d007ef114b71e451
ehsanfakhri <fakhri.ehsan@gmail.com>
2023-09-21 12:13:30 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import mypic from "./mypic.jpg";
+import styles from "./styles.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.card}>
+      <div className={styles.cardImage}>
+        <img width={400} height={400} src={mypic} alt="pic" />
+      </div>
+      <div className={styles.cardTitle}>
+        <h1>Ehsan Fakhri</h1>
+      </div>
     </div>
   );
 }
